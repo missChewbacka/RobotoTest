1fd0498afe98318b425ce03fd50a2e14cf03dad1
missChewbacka <alexandra@evolany.com>
2024-01-29 16:09:08 +0200

    test_demo_1

--- libraries/lib.bat
+++ libraries/lib.bat
@@ -1,5 +1,2 @@
 pip install robotframework
 pip install robotframework-SeleniumLibrary
-pip install robotframework-OperatingSystem
-pip install robotframework-XML
-pip install robotframework-DateTime
